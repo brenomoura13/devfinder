2274e6d624a732c4dea7fcf458beea121ecfc976
Breno Moura <59323654+brenomoura13@users.noreply.github.com>
2022-04-16 20:03:42 -0300

    fix: Responsividade na section do vídeo.

--- src/components/VideoSection/index.tsx
+++ src/components/VideoSection/index.tsx
@@ -11,16 +11,16 @@ const Footer: NextPage = () => {
   return (
     <>
       <h1 className="title is-size-1 is-family-code is-uppercase has-background-primary has-text-white has-text-centered is-underlined has-margin-top-minus-200 has-padding-20 has-text-weight-bold">Conheça a DevFinder</h1>
-      <section className="section is-medium is-mobile">
-        <div className="columns is-vcentered is-mobile is-centered">
+      <section className="section is-medium">
+        <div className="columns is-vcentered is-centered">
           <div className="column is-4">
             <div data-aos="flip-up" className="box has-background-primary">
               <iframe width="100%" height="320" src="https://www.youtube.com/embed/DXTPjzFYs4k" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
           </div>
-          <div className="column is-8 has-padding-left-90">
+          <div className="column is-8">
             <h1 data-aos="fade-down" className="title is-size-3">A maior plataforma para dar <b className="has-text-primary is-underlined">match</b></h1>
-            <p data-aos="fade-left" className="subtitle is-size-4">Explore ao máximo nossa plataforma para conseguir se destacar entre as empresas ou candidatos.</p>
+            <p data-aos="fade-right" className="subtitle is-size-4">Explore ao máximo nossa plataforma para conseguir se destacar entre as empresas ou candidatos.</p>
             <div className="columns is-align-items-stretch">
               <div className="column">
                 <div data-aos="zoom-in-up" className="box">

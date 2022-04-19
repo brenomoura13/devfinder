05c7b6843c73ebaca698c91e7d5880e239e7e658
Breno Moura <59323654+brenomoura13@users.noreply.github.com>
2022-04-19 19:38:59 -0300

    upt: Ajustado SEO da página com next-seo

---
+++ next-seo.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: 'https://www.url.ie/',
+    site_name: 'SiteName',
+  },
+  twitter: {
+    handle: '@handle',
+    site: '@site',
+    cardType: 'summary_large_image',
+  },
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from 'next/app'
 import '../../styles/global.scss'
 import 'aos/dist/aos.css'
+import { DefaultSeo } from 'next-seo'
+import SEO from '../../next-seo.config';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps}/>
     </>
   )
